ef57e45bc11283ad53c384de46a80148c2cb8c20
jessejkwk <janderson@nyatoday.com>
2018-06-26 16:02:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   get '/square/:number' do
     @square = params[:number]
-    @result = (@number.to_i**2)
-    return
+    @result = (@number.to_i**2).to_s
+    
   end
 end
