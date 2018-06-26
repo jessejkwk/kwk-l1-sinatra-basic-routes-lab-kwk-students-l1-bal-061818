84a8c368f99b8ed7fb988942e73e03a68210e4fd
jessejkwk <janderson@nyatoday.com>
2018-06-26 15:59:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,6 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   get '/square/:number' do
     @square = params[:number]
-    @result = #name.reverse
     return @result.to_i
   end
 end
